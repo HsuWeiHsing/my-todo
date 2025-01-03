eb8ff0d49231d2f00ea2f8fa1da99cacf1bc26da
HsuWeiHsing <gbcow0831@gmail.com>
2025-01-04 05:49:11 +0900

    2025-01-04  5:49

--- app/Http/Controllers/Home/TaskController.php
+++ app/Http/Controllers/Home/TaskController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\Task;
+// use Illuminate\Http\Request;
+// use App\Models\Task;
+use App\Models\User;
 
 class TaskController extends Controller
 {
     //
+    public function index() {
+        $users = User::all();
+        return view('task', ['users' => $users]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Home\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('/dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
