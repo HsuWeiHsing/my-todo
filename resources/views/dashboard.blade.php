e86d4295e7c2626c7ef1836840046402a3967a46
HsuWeiHsing <gbcow0831@gmail.com>
2025-01-04 16:37:09 +0900

    123

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,12 +3,10 @@
         <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
             {{ __('タスク一覧') }}
         </h2>
-        <br>
         <div class="font-semibold text-sm text-red-500 leading-tight">
             {{ __("ログイン中です。") }}
         </div>
     </x-slot>
-    
     <div>
         @foreach($tasks as $task)
             {{ $task->task_name }}
